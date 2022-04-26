d6c46272f3fc792953c522e879a22bde94c70081
Nikolas Alencar <nikolasalencarrs73@gmail.com>
2022-04-26 11:30:36 -0300

    fix(register): refatora função de criar usuário

--- src/app/services/register.service.ts
+++ src/app/services/register.service.ts
@@ -33,43 +33,56 @@ export class RegisterService {
   };
 
   createUser = (user: any, cod: number) => {
-    this.factoryService.obtemClienteByLogin(user.usuario)
-      .then(cliente => {
-        this.user = { ...cliente }
-      })
+    Promise.all([
+      this.factoryService.obtemClienteByLogin(user.usuario), 
+      this.factoryService.obtemClienteByEmail(user.email)
+    ]).then(([obtemByLogin, obtemByEmail]) => {
+      this.user = { ...obtemByLogin }
+      this.newUser = { ...obtemByEmail }
+    }).then(() => {
+      if(!this.validarUser(user)) {return}
+      if(!this.validarEmail(user)) {return}
+      this.subscriberService.hasExibirInput.next(true)
+      if(!this.validarCod(user, cod)) {return}
+      if(Number(cod) === this.cod){
+        this.success = true;
+        this.factoryService.createUser(user)
       .then(() => {
-        this.factoryService.obtemClienteByEmail(user.email)
-        .then(cliente => {
-          this.newUser = { ...cliente }
-          if(!this.autenticateService.validarLogin(user.usuario, user.senha) || this.user[0] !== undefined){
-            this.alertService.showAlertInfo(`O usuário ${user.usuario} já está cadastrado ou não é válido. Por favor, escolha outro usuário que possua entre 7 e 20 caracteres e não tenha caracteres especiais.`, { class: 'modal-xl'})
-            return
-          }
-          if(!this.autenticateService.validarEmail(user.email) || this.newUser[0] !== undefined){
-            this.alertService.showAlertInfo(`O usuário ${user.email} já está cadastrado ou não é válido. Por favor, escolha outro.`, { class: 'modal-xl'})
-            return
-          }
-          this.subscriberService.hasExibirInput.next(true)
-          if(cod === undefined || null){
-            this.alertService.showAlertInfo(this.opcoesCod[this.count])
-            this.count === 0 ? this.enviaEmailRegister(user.email) : this.count = 1;
-            return
-          }
-          if(Number(cod) === this.cod){
-            this.success = true;
-            this.factoryService.createUser(user)
-          .then(() => {
-            this.alertService.showAlertSuccess(`O usuário ${user.usuario} foi criado com sucesso!`)
-          })
-          .catch(e => console.log(e))
-          }else{
-            this.alertService.showAlertDanger('Código inválido!', { class: 'modal-sm'})
-          }
-        })
+        this.alertService.showAlertSuccess(`O usuário ${user.usuario} foi criado com sucesso!`)
       })
+      .catch(e => console.log(e))
+      }else{
+        this.alertService.showAlertDanger('Código inválido!', { class: 'modal-sm'})
+      }
+    })
   }
 
   enviaEmailRegister = (email: any) => {
     this.enviaMensagemService.enviaEmailRegister(email, this.cod)
+  }
+
+  validarUser = (user: any) => {
+    if(!this.autenticateService.validarLogin(user.usuario, user.senha) || this.user[0] !== undefined){
+      this.alertService.showAlertInfo(`O usuário ${user.usuario} já está cadastrado ou não é válido. Por favor, escolha outro usuário que possua entre 7 e 20 caracteres e não tenha caracteres especiais.`, { class: 'modal-xl'})
+      return false
+    }
+    return true
+  }
+
+  validarEmail = (user: any) => {
+    if(!this.autenticateService.validarEmail(user.email) || this.newUser[0] !== undefined){
+      this.alertService.showAlertInfo(`O email ${user.email} já está cadastrado ou não é válido. Por favor, escolha outro.`, { class: 'modal-xl'})
+      return false
+    }
+    return true
+  }
+
+  validarCod = (user:any, cod: any) => {
+    if(cod === undefined || null){
+      this.alertService.showAlertInfo(this.opcoesCod[this.count])
+      this.count === 0 ? this.enviaEmailRegister(user.email) : this.count = 1;
+      return false
+    }
+    return true
   }
 }
